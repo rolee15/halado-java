3dd996a867bd6cecaf16d52b5a3f83a86102c725
Roland Papdi <rolee15@gmail.com>
2024-03-19 20:47:15 +0100

    Cleaned up project

--- Lambda/src/test/GeometryTest.java
+++ Lambda/src/test/GeometryTest.java
@@ -4,11 +4,11 @@ import geometry.Plane;
 import geometry.Point;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GeometryTest {
 
--- Lambda/src/test/PrimeLambdaTest.java
+++ Lambda/src/test/PrimeLambdaTest.java
@@ -1,9 +1,7 @@
 package test;
 
 import lambda.PrimeLambda;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
